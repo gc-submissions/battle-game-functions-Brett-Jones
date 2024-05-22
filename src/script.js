db173d28bc52ac1dcaf5a5535a8ba57396c328f5
Kolstrike <kolstrike1@gmail.com>
2024-05-22 18:13:40 -0400

    finished debugging tests, everything works

--- src/script.js
+++ src/script.js
@@ -2,17 +2,14 @@
 
 // TODO - write your code here.
 
-
 const randomDamage = () => Math.floor(Math.random() * 10) + 1;
 
 const chooseOption = (opt1, opt2) => {
-    const randNum = Math.round(Math.random() );
+    const randNum = Math.round(Math.random());
     return randNum === 0 ? opt1 : opt2;
 }
 
-const attackPlayer = function(health) {
-    return health - randomDamage();
-}
+const attackPlayer = (health) => health - randomDamage();
 
 const logHealth = (player, health) => console.log(`${player} health: ${health}`);
 
@@ -20,40 +17,31 @@ const logDeath = (winner, loser) => console.log(`${winner} defeated ${loser}`);
 
 const isDead = (health) => health <= 0;
 
-function fight (
+const fight = (
     player1, 
     player2, 
     player1Health, 
-    player2Health) {
-	
-    let i = 1;
-	while (i < 2){
-        console.log(i);
-		i++;
-    
+    player2Health
+) => {
+	while (true) {
 		const attacker = chooseOption(player1, player2);
 
-    	if(attacker === player1){
+    	if (attacker === player1) {
 			player2Health = attackPlayer(player2Health);
         	logHealth(player2, player2Health);
-	
-        	if(isDead(player2Health)){
+        	if (isDead(player2Health)) {
         		logDeath(player1, player2);
         		break;
         	}
-		
-    	}else{
+    	} else {
     		player1Health = attackPlayer(player1Health);
         	logHealth(player1, player1Health);
-        	if(isDead(player1Health)){
+        	if (isDead(player1Health)) {
             	logDeath(player2, player1);
             	break;
 	        }
     	}
     }
-    
 }
 
 fight("Mitch", "Adam", 100, 100);
-
-
